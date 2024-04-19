4213832b0a260a5049f9ac411a46a4208a4000a7
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-04-20 00:08:33 +0300

    без 4 задачі

---
+++ src/generics/4.ts
@@ -0,0 +1,24 @@
+type UserType = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<UserType>): UserType {
+  const userData: User = {
+    name: "Bob",
+    surname: "Pikachu",
+    email: "pikapika@meta.ua",
+    password: "9379992",
+  };
+
+  return { ...userData, ...initialValues };
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
+
+export {};
